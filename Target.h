45766ad8377ae255f1a39823913bd09ad4fa39a2
Dan Olson <dano7900@gmail.com>
2019-04-27 16:22:24 -0400

    add target

---
+++ Target.h
@@ -0,0 +1,38 @@
+#ifndef Target_H
+#define Target_H
+
+#include "Arduino.h"
+#include "Filter.h"
+#include "SerialTransfer.h"
+
+#include "Configure.h"
+
+class Target {
+
+public:
+
+Target(uint8_t input,const Configure &set_config);
+
+Filter filter;
+SerialTransfer serial;
+
+void serial_print(bool serial_print);
+void onboard_address(uint16_t onboard_address);
+
+void switch_active();
+
+private:
+
+uint8_t _input;
+uint8_t _output;
+
+uint16_t _onboard_address;
+bool _serial_print;
+char const * _serial_message;
+bool _failsafe;
+
+bool _input_filter;
+
+};
+
+#endif
