0187f288a9e7e23addcde669db0c6e1190f0c70d
Dan Olson <dano7900@gmail.com>
2019-05-14 18:36:52 -0400

    update ino file

--- solenoid.ino
+++ solenoid.ino
@@ -1,39 +1,27 @@
-#include "pblib.h"
+#include "solenoid.h"
 
-uint8_t inputs_pullup[] = { 7,6,5,4,3,2};
-uint8_t outputs[] = { 12,11 };
+uint8_t inputs_pullup[] = { 7,6,5};
+uint8_t outputs[] = { 12 };
 
 PinConfig pins;
 
-//uint8_t tr_switches[] = { 7,6,5 };
-uint8_t dt_switches[] = { 7,6,5 };
+uint8_t switches[] = { 7,6,5 };
 
-Flipper flipper_left(4,12);
-//Trough trough(tr_switches,3,21,26,5);
-DropTarget drop_target(dt_switches,3,11);
-//Kicker kicker(16,18);
+Solenoid coil_device(switches,3,12);
 
 void setup() {
 
 Serial.begin(115200);
 
 pins.set_pins_input_pullup(inputs_pullup,6);
-pins.set_pins_pwm(outputs,2);
+pins.set_pins_pwm(outputs,1);
 
-flipper_left.hold_config(245,125,2500);
-drop_target.pwm_value(245);
-//kicker.pwm_value(245);
-//kicker.millis_config(2500,500);
+coil_device.pwm_value(245);
 
-//sys.set_wifi_sta_on();
-//Serial.println(sys.get_node_mac());
 
 }
 
 void loop() {
 
-flipper_left.coil_active();
-//Serial.println(trough.inv_count());
-//kicker.coil_active();
 Serial.println(drop_target.inv_targets_activated());
 }
