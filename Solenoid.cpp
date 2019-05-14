80ed3ae3f3bf76f3e002184883021842ff27f316
Dan Olson <dano7900@gmail.com>
2019-05-14 18:32:16 -0400

    update solenoid library conversion

--- Solenoid.cpp
+++ Solenoid.cpp
@@ -1,33 +1,102 @@
 #include "Arduino.h"
-#include "Solenoid.h"
+#include "Solenoid2.h"
 
-Solenoid::Solenoid() {
+Solenoid2::Solenoid2(uint8_t *input_pins,uint8_t num_pins,uint8_t output_pin) {
 
-}
+_output = output_pin;
+_serial_activated = false;
+_num_pins = num_pins;
+_filter_results[_num_pins];
+filter = new Filter();
+_count = 0;
+_pwm = 0;
 
-void Solenoid::on_pwm(uint8_t pin,uint8_t pwm) {
-
-analogWrite(pin,pwm);
-
-}
-
-void Solenoid::off_pwm(uint8_t pin) {
-
-analogWrite(pin,0);
-_reduce_pwm = false;
+memcpy(&_pins,&input_pins,sizeof _pins);
 
 }
 
-void Solenoid::on_pwm_reduce(uint8_t pin,uint8_t pwm,uint8_t pwm_hold,unsigned long millis_hold) {
-
-if(delay_state.wait_interval(millis_hold) == true) {
-_reduce_pwm = true;
+void Solenoid2::setSerialActivate() {
+_serial_activated = true;
 }
 
-if(_reduce_pwm == true) {
-analogWrite(pin,pwm_hold);
+void Solenoid2::activateOnInputs() {
+
+    if(numberOfInputsLow() >= _num_pins) {
+    analogWrite(_output,_pwm);
+    } else {
+    analogWrite(_output,0);
+    }
+    
+}
+
+void Solenoid2::activeOnInputReducePWM(uint32_t pwm_reduce,unsigned long millis_hold) {
+    _input_filter = filter.detect_edge(_input);
+
+if(_input_filter == true) {
+
+    if(delay_state.wait_interval(millis_hold) == true) {
+    _reduce_pwm = true;
+    }
+
+    if(_reduce_pwm == true) {
+    analogWrite(_input_pin,pwm_reduce);
+    } else {
+    analogWrite(_input_pin,_pwm);
+    }
+
 } else {
-analogWrite(pin,pwm);
+
+analogWrite(_input_pin,0);
+
+}
+ 
 }
 
+
+
+void Solenoid2::activateOnInputWait(unsigned long millis_wait,unsigned long millis_coil_on) {
+
+_input_filter = filter.detect_edge(_input);
+
+    if(filter.edge_status(_input_filter == true)) {
+    _millis_rec = millis();
+    }
+
+if(millis() <= millis_wait + _millis_rec) {
+
+    if(millis() >= ((millis_wait + _millis_rec) - millis_coil_on)) {
+    analogWrite(_output,_pwm);
+    }
+
+} else {
+analogWrite(_output,0);
+}
+
+if(_serial_activated == true) {
+    if(filter.edge_status(_input_filter) == true) {
+    serial.transmit(_message);
+    }
+}
+
+}
+
+uint8_t Solenoid2::numberOfInputsLow() {
+
+_count = 0;
+
+for(int i = 0; i < _num_pins; ++i) {
+
+_filter_results[i] = filter[i].detect_edge(_pins[i]);
+
+if(_filter_results[i] == true) {
+_count++;
+}
+}
+
+return _count;
+
+}
+
+void Solenoid2::pwm_value(uint32_t pwm_value) {
+_pwm = pwm_value;
 }
--- Solenoid.h
+++ Solenoid.h
@@ -3,23 +3,44 @@
 
 #include "Arduino.h"
 #include "DelayState.h"
-#include "PinConfig.h"
+#include "Filter.h"
+#include "SerialTransfer.h"
 
 class Solenoid {
 
 public:
 
-Solenoid();
+Solenoid(uint8_t *input_pins,uint8_t num_pins,uint8_t output);
 
-void on_pwm(uint8_t pin,uint8_t pwm);
-void off_pwm(uint8_t pin);
-void on_pwm_reduce(uint8_t pin,uint8_t pwm, uint8_t pwm_hold, unsigned long millis_hold);
-
+Filter *filter;
+SerialTransfer serial;
 DelayState delay_state;
+ 
+//void activateOnInput();
+void activateOnInputReducePWM(uint32_t pwm_reduce,unsigned long millis_hold); 
+void activateOnInputWait(unsigned long millis_wait,unsigned long millis_coil_on);
+void activateOnInputs();
+
+void setSerialActive();
+void setPwmValue(uint32_t pwm_value); 
+
+uint8_t numberOfInputsLow();
 
 private:
 
-bool _reduce_pwm;
+uint8_t _num_pins;
+uint8_t *_pins;
+uint8_t _count;
+
+uint8_t _input;
+uint8_t _output;
+
+bool _serial_activated;
+bool _filter_results[];
+
+unsigned long _millis_rec;
+
+uint32_t _pwm;
 
 };
 
