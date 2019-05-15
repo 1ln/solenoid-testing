2606da89404935d65293ba15d50b794554549d47
Dan Olson <dano7900@gmail.com>
2019-05-15 18:29:44 -0400

    update solenoid.h

--- solenoid.h
+++ solenoid.h
@@ -2,21 +2,11 @@
 #define pblib_H
 
 #include "Arduino.h"
-#include "CAN.h"
-#include "SPI.h"
-#include "SD.h"
 
 #include "Solenoid.h"
 #include "Filter.h"
 #include "PinConfig.h"
 #include "SerialTransfer.h"
-#include "CANSystem.h"
 #include "DelayState.h"
-#include "Flipper.h"
-#include "Trough.h"
-#include "Bumper.h"
-#include "Kicker.h"
-#include "DropTarget.h"
-#include "PlayfieldSwitch.h"
 
 #endif
