91ab999800df4a4e9a64212c58caea25abd4c26f
Dan Olson <dano7900@gmail.com>
2019-05-15 19:00:23 -0400

    test inputs

--- solenoid.ino
+++ solenoid.ino
@@ -1,20 +1,18 @@
 #include "solenoid.h"
 
-uint8_t inputs_pullup[] = { 7,6,5};
-uint8_t outputs[] = { 12 };
+uint8_t inputs_pullup[] = { 11,10,9};
+//uint8_t outputs[] = { 12 };
 
 PinConfig pins;
 
-uint8_t switches[] = { 7,6,5 };
-
-Solenoid coil_device(switches,3,12);
+Solenoid coil_device(inputs_pullup,3,12);
 
 void setup() {
 
 Serial.begin(115200);
 
-pins.set_pins_input_pullup(inputs_pullup,6);
-pins.set_pins_pwm(outputs,1);
+pins.set_pins_input_pullup(inputs_pullup,3);
+//pins.set_pins_pwm(outputs,1);
 
 coil_device.pwmValue(245);
 
